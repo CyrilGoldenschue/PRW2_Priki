f1decee0c664e359ffbf4824870b9f09800065db
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-11-30 13:53:12 +0100

    Add relation

--- app/Models/Practice.php
+++ app/Models/Practice.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Practice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'domain_id',
+        'publication_state_id',
+        'user_id',
+        'updated_at'
+    ];
+
+    public function domain()
+    {
+        return $this->belongsTo(Domain::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function publication_state()
+    {
+        return $this->belongsTo(PublicationState::class);
+    }
 }
