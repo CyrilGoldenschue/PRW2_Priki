b12ffa8edf1c4ffa1d6e03cf3f2270d97894c3db
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2021-12-02 10:29:53 +0100

    Copy bootstrap in public

---
+++ public/js/changeDomain.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************!*\
+  !*** ./resources/js/changeDomain.js ***!
+  \**************************************/
+var select = document.getElementById("domainList");
+var practices = document.getElementsByClassName("Practice");
+var table = document.getElementById("tablePractice");
+table.addEventListener("load", changeDomain);
+select.addEventListener("change", changeDomain);
+
+function changeDomain() {
+  console.log(select.value);
+}
+/******/ })()
+;
