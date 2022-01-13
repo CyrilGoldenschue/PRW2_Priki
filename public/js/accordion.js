8f999d69bb1315fe7c632e990d0c8a1dad4aca7f
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-13 09:07:18 +0100

    result of change on style and js

---
+++ public/js/accordion.js
@@ -0,0 +1,31 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***********************************!*\
+  !*** ./resources/js/accordion.js ***!
+  \***********************************/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+    /* Toggle between hiding and showing the active panel */
+
+    var panels = document.getElementsByClassName("panel");
+
+    for (x = 0; x < panels.length; x++) {
+      if (panels[x].dataset['opinion_comment'] == this.dataset['opinion']) {
+        if (panels[x].style.display === "table-row") {
+          panels[x].style.display = "none";
+        } else {
+          panels[x].style.display = "table-row";
+          panels[x].style.colspan = "3";
+        }
+      }
+    }
+  });
+}
+/******/ })()
+;
