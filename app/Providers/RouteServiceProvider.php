a4c7cc7d2e7f2c4e94777ec2eb41cc80fd9aad38
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-20 09:19:54 +0100

    Change login out

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/practices';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
