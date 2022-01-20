167a6b3b08f21fe56e5ea022b9b33ecc3a8d057a
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-20 08:27:52 +0100

    Add get id in the url

--- app/Http/Controllers/OpinionController.php
+++ app/Http/Controllers/OpinionController.php
@@ -4,16 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Opinion;
+use Illuminate\Support\Facades\Auth;
 
 class OpinionController extends Controller
 {
 
+    /**
+     * @param $id : id of the opinion on which the user wants to comment
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function create($id){
 
-    public function create(){
-        return view('add-comment');
+        return view('add-comment')->with(["id" => $id]);
     }
 
-    public function store(){
-
+    public function store(Request $request, $id){
+        $opinion = Opinion::find($id);
+        $opinion->comments()->attach(Auth::user(), ['comment' => $_GET['comment'], 'points' => $_GET['points']]);
+        return redirect('/practice/'.$opinion->practice->id)->with('success', "Commentaire enregistré");
     }
 }
