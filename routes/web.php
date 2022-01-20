1a6476f1301ffccb1308afcd148e56a778a2df93
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-20 09:20:13 +0100

    Add route to display all practices

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReferenceController;
 use App\Http\Controllers\OpinionController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PracticeController;
 
 Route::get('/', [HomeController::class, 'index'])->name("homePage");
-Route::get('/practice/{id}', [HomeController::class, 'practice']);
+Route::get('/home', [HomeController::class, 'index'])->name("homePage");
+Route::get('/practice/{id}', [PracticeController::class, 'show'])->middleware(['auth']);
 Route::get('/opinion/{id}/create', [OpinionController::class, 'create'])->middleware(['auth']);
-Route::get('/opinion/{id}/store', [OpinionController::class, 'store'])->middleware(['auth'])->name('opinion.store');
+Route::get('/opini on/{id}/store', [OpinionController::class, 'store'])->middleware(['auth'])->name('opinion.store');
 
 
-
-Route::get('/practices', [HomeController::class, 'index'])->middleware(['auth'])->name('practices');
+Route::get('/practices', [PracticeController::class, 'showAll'])->middleware(['auth']);
+Route::get('/practice', [HomeController::class, 'index'])->middleware(['auth'])->name('practice');
 Route::resource("references", ReferenceController::class)->middleware(['auth']);
 
 require __DIR__.'/auth.php';
