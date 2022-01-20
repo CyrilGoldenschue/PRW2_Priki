db612b099afcab28083a6b568846707965daf8d8
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-20 10:23:00 +0100

    Add gate to display all practices only for modo

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -6,23 +6,26 @@ use App\Models\Practice;
 use App\Models\PublicationState;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class PracticeController extends Controller
 {
     //
-    public function show($id){
+    public function show($id)
+    {
         Carbon::setLocale(config('app.locale'));
         $practice = Practice::find($id);
-        if($practice->publication_state->slug == "PUB") {
-            return view('practiceDetail')->with(["practice" => $practice]);
-        }else {
-            return redirect("/");
-        }
+        return view('practiceDetail')->with(["practice" => $practice]);
     }
 
-    public function showAll(){
+    public function showAll()
+    {
+        if (!Gate::allows('list-all-practices', Auth::user())) {
+            abort(403);
+        }
         Carbon::setLocale(config('app.locale'));
         $practices = Practice::all();
-        return view('list-practices')->with(["practices" => $practices]);
+        return view('practices')->with(["practices" => $practices]);
     }
 }
