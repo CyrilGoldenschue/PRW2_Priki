3018d3cf1ed678a8ab678093c2d4f98eafa18c4f
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-20 11:19:26 +0100

    Create subView to display all practices and practices by day

--- resources/views/list-practices.blade.php
+++ resources/views/list-practices.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    @if($script)
+    @if($practicesByDay)
         <div>
             <select id="domainList">
                 <option class="DomainOption"
@@ -21,39 +21,69 @@
     @endif
 
     <h2>Pratiques</h2>
-    @if($script)
-        <p>Mises à jour dans les <input type="number" id="Days" name="days" value="{{$nbDays}}" min="0" max="365"> jours </p>
+    @if($practicesByDay)
+        <p>Mises à jour dans les <input type="number" id="Days" name="days" value="{{$nbDays}}" min="0" max="365"> jours
+        </p>
     @endif
 
-        <table @if($script) id="tablePractice" @endif class="table">
+    <table @if($practicesByDay) id="tablePractice" @endif class="table">
         <tr>
             <th>Description</th>
-            <th class="Domain">Domaine</th>
+            @if($practicesByDay)
+                <th class="Domain">Domaine</th> @endif
             <th>Modifié le</th>
         </tr>
-            @if($script)
-                <a id="noPractice">Aucune pratique à afficher ici</a>
-            @endif
+        @if($practicesByDay)
+            <a id="noPractice">Aucune pratique à afficher ici</a>
+        @endif
+        @if($practicesByDay)
+            @foreach($practices as $practice)
 
-        @foreach($practices as $practice)
+                <tr onclick="document.location = '/practice/{{$practice->id}}'" @if($practicesByDay) class="Practice"
+                    data-date="{{$practice->updated_at}}" data-domain="{{ $practice->domain->name }}" @endif>
+                    <td>
+                        {{ $practice->description }}
+                    </td>
+                    @if($practicesByDay)
+                        <td class="Domain">
+                            {{ $practice->domain->name }}
+                        </td>
+                    @endif
+                    <td>
+                        {{\Carbon\Carbon::parse($practice->updated_at)->translatedFormat('d F Y')}}
+                    </td>
+                </tr>
 
-            <tr onclick="document.location = '/practice/{{$practice->id}}'"  @if($script) class="Practice"
-                data-date="{{$practice->updated_at}}" data-domain="{{ $practice->domain->name }}"  @endif>
-                <td>
-                    {{ $practice->description }}
-                </td>
-                <td class="Domain">
-                    {{ $practice->domain->name }}
-                </td>
-                <td>
-                    {{\Carbon\Carbon::parse($practice->updated_at)->translatedFormat('d F Y')}}
-                </td>
-            </tr>
+            @endforeach
+        @else
+            @foreach($practices as $domain)
+                <tr>
+                    <td><h2>{{ $domain->name }}</h2></td>
+                </tr>
+                @foreach($domain->practices->sortby('publication_state_id') as $practice)
 
-        @endforeach
+                    <tr onclick="document.location = '/practice/{{$practice->id}}'"
+                        @if($practicesByDay) class="Practice"
+                        data-date="{{$practice->updated_at}}" data-domain="{{ $practice->domain->name }}" @endif>
+                        <td>
+                            {{ $practice->description }}
+                        </td>
+                        @if($practicesByDay)
+                            <td class="Domain">
+                                {{ $practice->domain->name }}
+                            </td>
+                        @endif
+                        <td>
+                            {{\Carbon\Carbon::parse($practice->updated_at)->translatedFormat('d F Y')}}
+                        </td>
+                    </tr>
+
+                @endforeach
+            @endforeach
+        @endif
 
     </table>
-    @if($script)
+    @if($practicesByDay)
         <script src="{{ mix('js/changeDays.js') }}" defer></script>
     @endif
 @endsection
