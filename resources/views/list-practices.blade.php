ccdadb2908652d8dbf76c106e778ec38741eedc4
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-01-20 09:20:55 +0100

    Create blade to display all practices

--- resources/views/list-practices.blade.php
+++ resources/views/list-practices.blade.php
@@ -1,1 +1,24 @@
-<?php
+@extends('layout')
+
+@section('content')
+    <table class="table">
+        <tr>
+            <th>Description</th>
+            <th class="Domain">Domaine</th>
+            <th>Modifié le</th>
+        </tr>
+        @foreach($practices as $practice)
+            <tr>
+                <td>
+                    {{ $practice->description }}
+                </td>
+                <td>
+                    {{ $practice->domain->name }}
+                </td>
+                <td>
+                    {{\Carbon\Carbon::parse($practice->updated_at)->translatedFormat('d F Y')}}
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
